b22ca956168b0d23a2f2adf71ef0809a845ba051
bashar <121734679+mohd-mb2205153@users.noreply.github.com>
2025-03-21 19:22:42 +0300

    Update Networks.sh
    Added user friendly color coded echo messages

--- Scripts/VM1 - Server Side Tasks/Networks.sh
+++ Scripts/VM1 - Server Side Tasks/Networks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Note: This should run in a seperate dedicated terminal. To stop it, Press Ctrl + Z
+
 #Creating the directory if it doesnt exist
 mkdir -p PingDirectory
 
@@ -19,27 +21,25 @@ ping_client() {
 	#Modify permissions to read and write for owner only
 	chmod 600 "$outputFile"
 
+	echo -e "\n\033[1;34mPinging $client_name at $(date +"%Y-%m-%d %H:%M:%S")...\033[0m"
+
 	#Replace google.com with target VM IP address
 	ping "$client_name" -s 500 -c 10 >> "$outputFile"
+
+	echo -e "\033[1;32mPing results for $client_name saved to $outputFile\033[0m"
 
 }
 
 while true
 do
-	#ClientVM1
+	#ClientVM2
 	ping_client "google.com"
 
-	#ClientVM2
+	#ClientVM3 
 	ping_client "bing.com"
 
 	#Every 10 seconds interval
+	echo -e "\n\033[1;33mWaiting for 10 seconds before the next ping cycle...\033[0m"
 	sleep 10 
 done
-
-
-#Note: This should run in a seperate dedicated terminal. To stop it, Press Ctrl + Z
-
-
-
-
 
