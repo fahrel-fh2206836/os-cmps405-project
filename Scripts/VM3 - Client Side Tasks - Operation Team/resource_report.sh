dfc6babfdc04a79e974b8ec489f5549a24b85e8a
Fahrel <fahrelazki@gmail.com>
2025-03-21 18:55:53 +0300

    Update resource_report.sh

--- Scripts/VM3 - Client Side Tasks - Operation Team/resource_report.sh
+++ Scripts/VM3 - Client Side Tasks - Operation Team/resource_report.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Time and output paths
-TIMESTAMP=$(date "+%Y-%m-%d Time: %H-%M-%S")
-REPORT="/resource_report_$TIMESTAMP.txt"
-DEST_DIR="/var/operations/report"  # Must exist on VM1
-VM1_USER="admin"                       # Change to actual user
-VM1_IP="192.168.1.100"                 # Change to actual IP
-REMOTE_PATH="$VM1_USER@$VM1_IP:$DEST_DIR"
+REPORT="./resource_report_$(date "+%Y-%m-%d_%H-%M-%S").txt"
+#DEST_DIR="/var/operations/report"  # Must exist on VM1
+#VM1_USER="admin"                       # Change to actual user
+#VM1_IP="192.168.1.100"                 # Change to actual IP
+#REMOTE_PATH="$VM1_USER@$VM1_IP:$DEST_DIR"
 
 # 1. Create Report
 {
-    echo "======= Resource Report ($TIMESTAMP) ======="
+    echo "======= Resource Report $(date "+%Y-%m-%d Time: %H-%M-%S") ======="
     echo
     echo "--Process Tree--"
     pstree
@@ -35,10 +33,10 @@ REMOTE_PATH="$VM1_USER@$VM1_IP:$DEST_DIR"
 } > "$REPORT"
 
 # 2. Securely copy the report to VM1
-scp "$REPORT" "$REMOTE_PATH"
+#scp "$REPORT" "$REMOTE_PATH"
 
 # 3. Delete local report
-rm "$REPORT"
+#rm "$REPORT"
 
 ## Automate hourly using crontab
 ## crontab -e
