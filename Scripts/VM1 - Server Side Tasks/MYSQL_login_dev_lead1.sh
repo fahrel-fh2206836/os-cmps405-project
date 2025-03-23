6f65983ddf002fa3103cc3fdfedddffbf3c81fb9
calma21 <joshcalma21@yahoo.com>
2025-03-24 00:17:10 +0300

    updated MYSQL_login_<USER_NAME>.sh

--- Scripts/VM1 - Server Side Tasks/MYSQL_login_dev_lead1.sh
+++ Scripts/VM1 - Server Side Tasks/MYSQL_login_dev_lead1.sh
@@ -1,57 +1,43 @@
-##Installation of MySQL
-sudo apt update;
-sudo apt install mysql-server;
-sudo systemctl start myql;
-sudo mysql_secure_installation;
+## Before Script: MySQL Setup
 
-##Obtain IP address of Server
-SERVER_IP=$(ip -4 addr show | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | grep -v '127.0.0.1' | head -n 1);
+# Installation of MySQL
+# sudo apt update;
+# sudo apt install mysql-server;
+# sudo systemctl start mysql;
+# sudo mysql_secure_installation;
 
-##Creation of Users and Databases
-##Assigning Privileges
-sudo mysql -u root <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root_pass_123';
-FLUSH PRIVILEGES;
+#sudo mysql -u root
+#In MySQL:
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root_pass_123'; Can change 'root_pass_123' to any password
+#FLUSH PRIVILEGES;
 
-CREATE USER IF NOT EXISTS 'dev_lead1'@'%' IDENTIFIED BY 'dev123';
+#sudo systemctl restartmysql;
 
-CREATE DATABASE IF NOT EXISTS development_db;
+## MySQL Firewall Configuration
+# sudo ufw allow 3306/tcp;
+# sudo ufw reload;
 
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE VIEW, SHOW VIEW ON development_db TO 'dev_lead1'@'%';
+## Configure MySQL to Accept Remote Connections
+# Modify MySQL configuration to allow remote connections by setting bind-address to 0.0.0.0
+# sudo sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf;
+# sudo systemctl restart mysql;
 
-FLUSH PRIVILEGES;
-EOF
+## Enable General Query Log for Monitoring
+# sudo touch "/var/log/mysql/general.log";
+# sudo chmod 666 "/var/log/mysql/general.log";
+# sudo touch "/var/log/mysql/mysql_monitor.log";
+# sudo chmod 666 "/var/log/mysql/mysql_monitor.log";
 
-#Verify User Creation
-mysql -u root -p 'root_pass_123' -e "SELECT User FROM mysql.user;"
+# sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
+#[mysqld]
+#general_log = 1
+#general_log_file = /var/log/mysql/general.log
 
-## dev_lead1 Authentication and Verification | Database and Table Exploration
-sudo mysql -u dev_lead1 -p'dev123' -h $SERVER_IP <<EOF
-SELECT USER(), CURRENT_USER();
-SHOW SESSION STATUS;
-SHOW DATABASES;
+# Restart MySQL to apply logging configuration
+# sudo systemctl restart mysql;
 
--- Loop through each accessible database and show tables
-SHOW TABLES IN development_db;
-EOF
-
-#Logging
-sudo sed -i '/\[mysqld\]/a general_log = 1\ngeneral_log_file = /var/log/mysql/general.log' "/etc/mysql/my.cnf"
-sudo systemctl restart mysql;
-
-tail -f "/var/log/mysql/general.log" | while read line; do
-	#Logins
-    if [[ "$line" == *"Connect"* && "$line" == *"dev_lead1"* ]]; then
-        echo "$(date) - LOGIN: $line" >> "/var/log/mysql/mysql_monitor.log"
-    fi
-
-   	#Queries
-    if [[ "$line" == *"Query:"* && "$line" == *"dev_lead1"* ]]]; then
-        echo "$(date) - QUERY: $line" >> "/var/log/mysql/mysql_monitor.log"
-    fi
-done
-
-# sudo bash -c "cat > /etc/systemd/system/mysql_log.service" << EOF
+##Creating systemd service for automation
+# sudo nano /etc/systemd/system/mysql_log_dev_lead1.service
 # [Unit]
 # Description=MySQL Log Monitoring Service
 # After=network.target
@@ -63,9 +49,53 @@ done
 
 # [Install]
 # WantedBy=multi-user.target
-# EOF
+# 
 
+##Reload Systemd for new service
 # sudo systemctl daemon-reload;
 
+#Enable and Start Service on Boot;
 # sudo systemctl enable mysql_log.service;
 # sudo systemctl start mysql_log.service;
+
+##Obtain IP address of Server
+SERVER_IP=$(hostname -I | awk '{print $1}')
+
+##Creation of Users and Databases
+##Assigning Privileges
+sudo mysql -u root -p<<EOF
+
+CREATE USER IF NOT EXISTS 'dev_lead1'@'%' IDENTIFIED BY 'dev123';
+
+CREATE DATABASE IF NOT EXISTS development_db;
+
+GRANT ALL PRIVILEGES ON development_db.* TO 'dev_lead1'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+
+#Verify User Creation
+mysql -u root -p -e "SELECT User FROM mysql.user;"
+
+## dev_lead1 Authentication and Verification | Database and Table Exploration
+sudo mysql -u dev_lead1 -p -h $SERVER_IP <<EOF
+SELECT USER(), CURRENT_USER();
+SHOW SESSION STATUS;
+SHOW DATABASES;
+
+-- Loop through each accessible database and show tables
+SHOW TABLES IN development_db;
+EOF
+
+#Logging
+tail -f "/var/log/mysql/general.log" | while read line; do
+	#Logins
+    if [[ "$line" == *"Connect"* && "$line" == *"dev_lead1"* ]]; then
+        echo "$(date) LOGIN: $line" >> "/var/log/mysql/mysql_monitor.log"
+    fi
+
+   	#Queries
+    if [[ "$line" == *"Query"* ]]; then
+        echo "$(date) QUERY: $line" >> "/var/log/mysql/mysql_monitor.log"
+    fi
+done	
--- Scripts/VM1 - Server Side Tasks/MYSQL_login_ops_lead1.sh
+++ Scripts/VM1 - Server Side Tasks/MYSQL_login_ops_lead1.sh
@@ -1,72 +1,101 @@
-##Installation of MySQL
-sudo apt update;
-sudo apt install mysql-server;
-sudo systemctl start myql;
-sudo mysql_secure_installation;
+## Before Script: MySQL Setup
 
-##Obtain IP address of Server
-SERVER_IP=$(ip -4 addr show | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | grep -v '127.0.0.1' | head -n 1);
+# Installation of MySQL
+# sudo apt update;
+# sudo apt install mysql-server;
+# sudo systemctl start mysql;
+# sudo mysql_secure_installation;
 
-##Creation of Users and Databases
-##Assigning Privileges
-sudo mysql -u root <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root_pass_123';
-FLUSH PRIVILEGES;
+#sudo mysql -u root
+#In MySQL:
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root_pass_123'; Can change 'root_pass_123' to any password
+#FLUSH PRIVILEGES;
 
-CREATE USER IF NOT EXISTS 'ops_lead1'@'%' IDENTIFIED BY 'ops123';
+#sudo systemctl restartmysql;
 
-CREATE DATABASE IF NOT EXISTS operations_db;
+## MySQL Firewall Configuration
+# sudo ufw allow 3306/tcp;
+# sudo ufw reload;
 
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE VIEW, SHOW VIEW ON operations_db TO 'ops_lead1'@'%';
+## Configure MySQL to Accept Remote Connections
+# Modify MySQL configuration to allow remote connections by setting bind-address to 0.0.0.0
+# sudo sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf;
+# sudo systemctl restart mysql;
 
-FLUSH PRIVILEGES;
-EOF
+## Enable General Query Log for Monitoring
+# sudo touch "/var/log/mysql/general.log";
+# sudo chmod 666 "/var/log/mysql/general.log";
+# sudo touch "/var/log/mysql/mysql_monitor.log";
+# sudo chmod 666 "/var/log/mysql/mysql_monitor.log";
 
-#Verify User Creation
-mysql -u root -p 'root_pass_123' -e "SELECT User FROM mysql.user;"
+# sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
+#[mysqld]
+#general_log = 1
+#general_log_file = /var/log/mysql/general.log
 
-## ops_lead1 Authentication and Verification | Database and Table Exploration
-sudo mysql -u ops_lead1 -p'ops123' -h $SERVER_IP <<EOF
-SELECT USER(), CURRENT_USER();
-SHOW SESSION STATUS;
-SHOW DATABASES;
+# Restart MySQL to apply logging configuration
+# sudo systemctl restart mysql;
 
--- Loop through each accessible database and show tables
-SHOW TABLES IN development_db;
-SHOW TABLES IN operations_db;
-EOF
-
-#Logging
-sudo sed -i '/\[mysqld\]/a general_log = 1\ngeneral_log_file = /var/log/mysql/general.log' "/etc/mysql/my.cnf"
-sudo systemctl restart mysql;
-
-tail -f "/var/log/mysql/general.log" | while read line; do
-	#Logins
-    if [[ "$line" == *"Connect"* && "$line" == *"ops_lead1"* ]]; then
-        echo "$(date) - LOGIN: $line" >> "/var/log/mysql/mysql_monitor.log"
-    fi
-
-   	#Queries
-    if [[ "$line" == *"Query:"* && "$line" == *"ops_lead1"* ]]; then
-        echo "$(date) - QUERY: $line" >> "/var/log/mysql/mysql_monitor.log"
-    fi
-done
-
-# sudo bash -c "cat > /etc/systemd/system/mysql_log.service" << EOF
+##Creating systemd service for automation
+# sudo nano /etc/systemd/system/mysql_log_ops_lead1.service
 # [Unit]
 # Description=MySQL Log Monitoring Service
 # After=network.target
 
 # [Service]
-# ExecStart=/bin/bash /path/to/your/script/MySQL_login_user_name.sh
+# ExecStart=/bin/bash /path/to/your/script/MySQL_login_ops_lead1.sh
 # Restart=always
 # User=root
 
 # [Install]
 # WantedBy=multi-user.target
-# EOF
+# 
 
+##Reload Systemd for new service
 # sudo systemctl daemon-reload;
 
+#Enable and Start Service on Boot;
 # sudo systemctl enable mysql_log.service;
 # sudo systemctl start mysql_log.service;
+
+##Obtain IP address of Server
+SERVER_IP=$(hostname -I | awk '{print $1}')
+
+##Creation of Users and Databases
+##Assigning Privileges
+sudo mysql -u root -p<<EOF
+
+CREATE USER IF NOT EXISTS 'ops_lead1'@'%' IDENTIFIED BY 'ops123';
+
+CREATE DATABASE IF NOT EXISTS operations_db;
+
+GRANT ALL PRIVILEGES ON operations.* TO 'ops_lead1'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+
+#Verify User Creation
+mysql -u root -p -e "SELECT User FROM mysql.user;"
+
+## ops_lead1 Authentication and Verification | Database and Table Exploration
+sudo mysql -u ops_lead1 -p -h $SERVER_IP <<EOF
+SELECT USER(), CURRENT_USER();
+SHOW SESSION STATUS;
+SHOW DATABASES;
+
+-- Loop through each accessible database and show tables
+SHOW TABLES IN operations_db;
+EOF
+
+#Logging
+tail -f "/var/log/mysql/general.log" | while read line; do
+	#Logins
+    if [[ "$line" == *"Connect"* && "$line" == *"ops_lead1"* ]]; then
+        echo "$(date) LOGIN: $line" >> "/var/log/mysql/mysql_monitor.log"
+    fi
+
+   	#Queries
+    if [[ "$line" == *"Query"* ]]; then
+        echo "$(date) QUERY: $line" >> "/var/log/mysql/mysql_monitor.log"
+    fi
+done	
