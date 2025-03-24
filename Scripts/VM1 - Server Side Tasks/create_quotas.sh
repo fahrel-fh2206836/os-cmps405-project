1868af85914ee26a34823b6625c3e72b51be05c5
Fahrel <fahrelazki@gmail.com>
2025-03-24 18:55:09 +0300

    Update create_quotas.sh

--- Scripts/VM1 - Server Side Tasks/create_quotas.sh
+++ Scripts/VM1 - Server Side Tasks/create_quotas.sh
@@ -5,8 +5,6 @@ sudo apt-get install quota quotatool
 mkdir -p /shared
 chmod 777 /shared
 
-# Need to configure a seperate partition for /shared
-
 # Quota limits (in KB)
 ONE_GB=$((1024 * 1024))
 SOFT_dev=$((5 * ONE_GB))  
@@ -17,26 +15,26 @@ HARD_ops=$((4 * ONE_GB))
 
 echo "Enabling quota support..."
 
-# Ensure /shared is mounted with usrquota
-mount | grep /shared | grep -q usrquota
+# Ensure / is mounted with usrquota
+mount | grep / | grep -q usrquota
 if [ $? -ne 0 ]; then
     sed -i 's/errors=remount-ro/&,usrquota/' /etc/fstab
-    mount -o remount,usrquota /shared
+    mount -o remount,usrquota /
 else
-    echo "✅ usrquota already enabled on /shared"
+    echo "✅ usrquota already enabled on /"
 fi
 
 # Initialize quota files
 echo "Running quotacheck..."
-quotacheck -cum /shared
+quotacheck -cum /
 
 # Turn on quota
 echo "Turning quota on..."
-quotaon /shared
+quotaon /
 # Apply quotas to users
 echo "Setting quota limits on dev_lead1 & ops_lead1..."
 
-setquota -u dev_lead1 $SOFT_dev $HARD_dev 0 0 /shared
-setquota -u ops_lead1 $SOFT_ops $HARD_ops 0 0 /shared
+setquota -u dev_lead1 $SOFT_dev $HARD_dev 0 0 /
+setquota -u ops_lead1 $SOFT_ops $HARD_ops 0 0 /
 
 echo "✅ Quotas applied"
